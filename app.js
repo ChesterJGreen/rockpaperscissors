950ab0de1c6fbc5f332f182aa17873ffc43b72c8
cjgreenie <seejarom@gmail.com>
2021-06-30 17:01:35 -0600

    almost visually done

--- app.js
+++ app.js
@@ -16,30 +16,35 @@ let c = 'TIE GAME'
 
 let compChoice = 0
 function play(playerChoice) {
+  document.getElementById('a').src = ''
+  document.getElementById('b').src = ''
+  document.getElementById('c').src = ''
+  document.getElementById('d').src = ''
+
   console.log(playerChoice)
   compChoice = Math.floor(Math.random() * (3));
   console.log(compChoice);
   if (playerChoice === compChoice) {
+    document.getElementById('c').src = "https://png.pngtree.com/png-vector/20191121/ourmid/pngtree-right-orange-tie-vector-or-color-illustration-png-image_2012829.jpg?w=300&h=300"
     window.alert(c)
-    document.getElementById('c').src = "https://png.pngtree.com/png-vector/20191121/ourmid/pngtree-right-orange-tie-vector-or-color-illustration-png-image_2012829.jpg"
   }
   else if (playerChoice === 0 && compChoice === 2) {
+    document.getElementById('a').src = "https://beardeertshirt.files.wordpress.com/2011/09/rock-paper-scissors-shirt.jpg?w=300&h=300"
     window.alert(a)
-    console.log(a)
   }
   else if (playerChoice === 2 && compChoice === 0) {
+    document.getElementById('a').src = "https://beardeertshirt.files.wordpress.com/2011/09/rock-paper-scissors-shirt.jpg?w=300&h=300"
     window.alert(b)
-    console.log(b)
+
   }
-  else if (playerChoice > compChoice) {
+  else if (playerChoice === 1 && compChoice === 0 || playerChoice === 0 && compChoice === 1) {
+    document.getElementById('a').src = "https://www.researchgate.net/publication/341163556/figure/fig1/AS:887880457142275@1588698605304/A-tree-growing-through-a-rock.png?w=300&h=300"
     window.alert(a)
-    console.log(a)
   }
   else {
+    document.getElementById('d').src = "https://www.nicepng.com/png/detail/307-3077975_hand-with-scissors-cutting-royalty-free-vector-scissors.png?w=300&h=300"
     window.alert(b)
-    console.log(b)
   }
-  winner()
 }
 
 
