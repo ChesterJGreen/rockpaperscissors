00ee157773aa31e21a4bcdb6487c74d3c2020172
cjgreenie <seejarom@gmail.com>
2021-06-30 16:26:56 -0600

    logic is mostly good

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
-let rock = 0;
-let paper = 1;
-let scissors = 2;
 
-function play(choice) {
-  let playerChoice = 'button';
-  let compChoice = 'rock'
+let rock = 0
+let paper = 1
+let scissors = 2
+let a = 'YOU WIN'
+let b = 'YOU LOSE'
+let c = 'TIE GAME'
+// const item= {
+//   rock: {
+//     name: "Rock",
+//     icon: "fas fa-gem",
+//     beats: ""
+//   }
+// }
+
+
+let compChoice = 0
+function play(playerChoice) {
+  console.log(playerChoice)
+  compChoice = Math.floor(Math.random() * (3));
+  console.log(compChoice);
   if (playerChoice === compChoice) {
-    console.log('Tie Game')
+    window.alert(c)
+    document.getElementById('c').src = "https://png.pngtree.com/png-vector/20191121/ourmid/pngtree-right-orange-tie-vector-or-color-illustration-png-image_2012829.jpg"
   }
   else if (playerChoice === 0 && compChoice === 2) {
-    window.alert('YOU WIN')
+    window.alert(a)
+    console.log(a)
   }
   else if (playerChoice === 2 && compChoice === 0) {
-    window.alert('YOU LOSE')
+    window.alert(b)
+    console.log(b)
   }
   else if (playerChoice > compChoice) {
-    window.alert('YOU WIN')
+    window.alert(a)
+    console.log(a)
   }
-  else (playerChoice < compChoice) {
-    window.alert('YOU LOSE')
+  else {
+    window.alert(b)
+    console.log(b)
   }
-
+  winner()
 }
 
-function rock() {
-  console.log('rock')
-  document.getElementById("rock").innerText = "You chose ROCK"
-  document.getElementById('animal-pic').innerHTML = '<img class="w-33" src="assets/duck.jpg" alt="duck">
+
+// function rock() {
+//   console.log('rock')
+//   document.getElementById("rock").innerText = "You chose ROCK"
+//   document.getElementById('animal-pic').innerHTML = '<img class="w-33" src="//placeholder" alt="">
+// }
